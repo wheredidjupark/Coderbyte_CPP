49f57e42a5a4853ba7f10432ca5a225fe89c0478
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-09 12:44:58 -0800

    add challenge1.cpp

---
+++ easy/challenge1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+string FirstReverse(string str) {
+    
+    // code goes here
+    return str;
+    
+}
+
+int main() {
+    
+    // keep this function call here
+    cout << FirstReverse(gets(stdin));
+    return 0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
