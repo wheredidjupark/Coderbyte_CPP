7bb6c777ea1bad6d58f10ee7878efd1394fec6ba
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-09 12:52:22 -0800

    add challenge3.cpp

---
+++ easy/challenge3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+string LongestWord(string sen) {
+    
+    // code goes here
+    return sen;
+    
+}
+
+int main() {
+    
+    // keep this function call here
+    cout << LongestWord(gets(stdin));
+    return 0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
