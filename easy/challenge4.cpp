e3d82cd6d041ffd9def51897e2e7a85c93387507
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-09 13:31:35 -0800

    add challenge4.cpp

---
+++ easy/challenge4.cpp
@@ -0,0 +1,23 @@
+/*
+ Using the C++ language, have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
+ 
+ Use the Parameter Testing feature in the box below to test your code with different arguments.
+ */
+
+#include <iostream>
+using namespace std;
+
+string LetterChanges(string str) {
+    
+    // code goes here
+    return str;
+    
+}
+
+int main() {
+    
+    // keep this function call here
+    cout << LetterChanges(gets(stdin));
+    return 0;
+    
+}
