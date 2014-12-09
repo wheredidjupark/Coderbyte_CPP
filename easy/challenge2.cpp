2964dfbee4de02801603c4ad22c9dcd6859616ad
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-09 12:49:00 -0800

    add challenge2.cpp

---
+++ easy/challenge2.cpp
@@ -0,0 +1,38 @@
+/*
+ Using the C++ language, have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18.
+ 
+ Use the Parameter Testing feature in the box below to test your code with different arguments.
+ */
+
+#include <iostream>
+using namespace std;
+
+int FirstFactorial(int num) {
+    
+    // code goes here
+    return num;
+    
+}
+
+int main() {
+    
+    // keep this function call here
+    cout << FirstFactorial(gets(stdin));
+    return 0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
