846b35390b84b3bb0f145a0998b377beceee329e
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-09 12:56:20 -0800

    add FindFirstWord function

--- easy/challenge3.cpp
+++ easy/challenge3.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
 
-string LongestWord(string sen) {
-    
-    // code goes here
-    return sen;
-    
-}
+string LongestWord(string sen);
+string FindFirstWord(string sen);
+
 
 int main() {
     
@@ -20,6 +18,61 @@ int main() {
 
 
 
+
+#include <iostream>
+#include <cctype>
+#include <string>
+using namespace std;
+
+string LongestWord(string sen);
+string FindFirstWord(string sen);
+
+int main() {
+    
+    // keep this function call here
+    cout << LongestWord(gets(stdin));
+    return 0;
+    
+}
+
+string LongestWord(string sen)
+{
+    return sen;
+}
+
+
+string FindFirstWord(string sen)
+{
+    int word_size =0; //size of the first word
+    int word_pos =0;
+    
+    int i =0;
+    while(i < sen.length())
+    {
+        if(isalpha(sen[i]))
+        {
+            word_pos = i;
+            word_size++;
+            i++;
+            while(i < sen.length())
+            {
+                if(isalpha(sen[i]))
+                {
+                    word_size++;
+                }
+                else
+                    return sen.substr(word_pos, word_size);
+            }
+        }
+        //else... continue with the loop
+        i++;
+        
+        
+        
+    }
+    
+    return sen.substr(word_pos, word_size);
+}
 
 
 
