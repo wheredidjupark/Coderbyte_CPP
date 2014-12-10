efc611d268ffb8221bfff8489c3cfff28b10ed0e
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-09 16:33:52 -0800

    touch challenge9.cpp

---
+++ easy/challenge9.cpp
@@ -0,0 +1,35 @@
+/*Using the C++ language, have the function TimeConvert(num) take the num parameter being passed and return the number of hours and minutes the parameter converts to (ie. if num = 63 then the output should be 1:3). Separate the number of hours and minutes with a colon.
+ 
+ Use the Parameter Testing feature in the box below to test your code with different arguments.*/
+#include <iostream>
+using namespace std;
+
+int TimeConvert(int num) {
+    
+    // code goes here
+    return num;
+    
+}
+
+int main() {
+    
+    // keep this function call here
+    cout << TimeConvert(gets(stdin));
+    return 0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
